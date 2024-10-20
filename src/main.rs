8e606fcd2f61061dc39112eac2186e37787445cf
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-20 00:49:07 -0400

    added comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    /*	
+        Step 1:
+        Create empty cli commands: larn init, larn commit, and larn learn
+        Step 2
+        Implement helper to store code changes ->git diff > changes.patch and git add .
+        Step 3
+        Implement helper to LLM calls and store responses in file
+        Step 4
+        Implement larn commit
+        Step 5 
+        Implement larn learn
+        Step 6
+        Implement larn 6
+        Step 7
+        Deploy rust package
+    */
+    println!("Let's Larn!");
 }
