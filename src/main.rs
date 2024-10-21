a40b96be25d83a3e9656a4c1bba779477171ab45
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-20 21:02:56 -0400

    write to file

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,29 @@
 use clap::Parser;
 mod commands;
 mod utils;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Parser)]
 struct Cli {
-    command:String,
+    command: String,
 }
 
 #[tokio::main]
-async fn main() {
-    /*	
-        Step 1:
-        Create empty cli commands: larn init, larn add, and larn learn [Done]
-        Step 2
-        Implement helper to store code changes ->git diff and git add .[Done]
-        Step 3
-        Implement helper to make LLM calls[Done]
-        Step 4
-        Implement helper to write to Redis[Done]
-        Step 5
-        Implement function to read all feedback in Redis and pass them to LLM for finding trends in coding mistakes and giving excercises to fix those issues[Done]
-        Step 6
-        Deploy rust package
-    */
-    
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
-   if args.command == "add"{
+
+    if args.command == "add" {
         let _ = commands::add::add().await;
+    }else if args.command == "learn" {
+        let output = commands::learn::learn().await.expect("Failed to execute learn command");
+        let mut file = File::create("output.txt")?;
+        file.write_all(output.as_bytes())?;
     }
-    else if args.command == "learn"{
-        let output = commands::learn::learn().await;
-        print!("{:?}",output);
-    }
-    else{
-        println!("{} is not a valid command" , args.command);
+    else {
+        println!("{} is not a valid command", args.command);
     }
 
+    Ok(())
 }
